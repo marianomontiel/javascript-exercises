1c3705a717829a9f161cc2f6d14a74f611ab86d5
marianomontiel <67610069+marianomontiel@users.noreply.github.com>
2023-01-17 15:11:28 -0300

    Finished

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,7 @@
-const repeatString = function() {
-
-};
+const repeatString = function(a,b) {
+        return a.repeat(b);
+    };
+repeatString('hey', 3);
 
 // Do not edit below this line
 module.exports = repeatString;
