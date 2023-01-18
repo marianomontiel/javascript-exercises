0addcd18c6260d4d6ce734aa4f0b5bff3160192f
marianomontiel <67610069+marianomontiel@users.noreply.github.com>
2023-01-18 10:30:56 -0300

    Define formula v1 for sumAll.js

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,9 @@
-const sumAll = function() {
-
+const sumAll = function(a,b) {
+  if (a - b > 0) {
+  const x = ((a-b)+1)*(a+b)/2;
+  return x;} else {  
+    const x = ((b-a)+1)*(a+b)/2;
+    return x;}
 };
 
 // Do not edit below this line
