3cc678e83e8db135af620e24816eab14f646da88
marianomontiel <67610069+marianomontiel@users.noreply.github.com>
2023-01-18 16:29:03 -0300

    Fix ERROR when using string as input.

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,9 +1,14 @@
-const sumAll = function(a,b) {
-  if (a - b > 0) {
-  const x = ((a-b)+1)*(a+b)/2;
-  return x;} else {  
-    const x = ((b-a)+1)*(a+b)/2;
-    return x;}
+const sumAll = function (a, b) {
+  let n = typeof 0;
+  if (a < 0 || b < 0 || (typeof a != n) || (typeof b != n) || isNaN(a) || isNaN(b)) {
+    return "ERROR";
+  } else if (a - b > 0) {
+    const x = ((a - b) + 1) * (a + b) / 2;
+    return x;
+  } else {
+    const x = ((b - a) + 1) * (a + b) / 2;
+    return x;
+  }
 };
 
 // Do not edit below this line
