8c50ec75a061eb3174970c519fec3acc2975ca64
marianomontiel <67610069+marianomontiel@users.noreply.github.com>
2023-01-18 11:17:11 -0300

    Define leapYears()

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,8 @@
-const leapYears = function() {
-
+//Leap years are divisible by 4. now, if the year is divisible by 100 it must also be divisible by 400 otherwise it is not a leap year
+const leapYears = function(a) {
+  if ((a % 4 && a % 100 && a % 400 === 0) || (a % 100 != 0 && a % 4 ===0) ) { 
+    return true;
+  } else { return false;};
 };
 
 // Do not edit below this line
