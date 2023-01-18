6885f3412777267be801a147e9de7d1cbd7450de
marianomontiel <67610069+marianomontiel@users.noreply.github.com>
2023-01-18 12:01:22 -0300

    Define tempConversion.js functions

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,13 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(a) {
+//  x °F ≘ (x − 32) × 5/9 °C
+  const b = (a - 32) * 5/9;
+  return Math.round( b * 10) / 10;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(a) {
+//    x °C ≘ (x × 9/5 + 32) °F
+  const b = (a * 9/5) + 32;
+  return Math.round( b * 10) / 10;
 };
 
 // Do not edit below this line
