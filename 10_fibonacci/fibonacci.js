419495354e90e349232b046663e0cd0e5eae7a33
marianomontiel <67610069+marianomontiel@users.noreply.github.com>
2023-02-13 17:09:06 -0300

    finished

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,16 @@
-const fibonacci = function() {
-
+const fibonacci = function (a) {
+    if (a < 0) { return 'OOPS' } else {
+        let fiboNum = Array(a.lenght);
+        fiboNum[0] = 1;
+        fiboNum[1] = 1;
+        for (let i = 2; i < a; i++) {
+            fiboNum[i] = fiboNum[i - 1] + fiboNum[i - 2];
+        }
+        const answer = fiboNum[fiboNum.length - 1];
+        return answer
+    }
 };
+fibonacci(10);
 
 // Do not edit below this line
 module.exports = fibonacci;
