b3d3105699818c4887724c5f935b030db274d668
marianomontiel <67610069+marianomontiel@users.noreply.github.com>
2023-02-14 14:06:32 -0300

    Main function but lacks additional functionality

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,10 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+  people.sort(function(a,b) {
+    const firstguy = a.yearOfDeath - a.yearOfBirth;
+    const nextguy = b.yearOfDeath - b.yearOfBirth;
+    return (firstguy > nextguy) ? 1:-1;
+    });
+    return people[0]
 };
 
 // Do not edit below this line
